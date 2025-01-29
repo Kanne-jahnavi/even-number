214b4bde71fb84442bc9fe5fc93c1961c1ad2e9f
Kanne-jahnavi <jahnavi12.27@gmail.com>
2025-01-29 18:53:51 +0530

    Add files via upload
    this program displays even numbers

---
+++ Even.java
@@ -0,0 +1,15 @@
+public class Even{
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 1;
+
+        while (num <= 100) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
+            num++;
+        }
+
+        System.out.println("Sum of even numbers between 1 and 100: " + sum);
+    }
+}
